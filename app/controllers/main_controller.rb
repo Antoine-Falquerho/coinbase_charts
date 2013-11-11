35ad41d17dd83060bb77e085f49a4aae429575cd
Antoine Falquerho <antoine.falquerho@gmail.com>
2013-11-10 16:55:18 -0800

    add google api charts

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,8 @@
 class MainController < ApplicationController
 
 	def index
-		@bitcoins = Bitcoin.all.order(:created_at).select(:buy_price, :sell_price).collect{|bitcoin| [bitcoin.buy_price, bitcoin.sell_price]}
+		@bitcoins = Bitcoin.all.order(:created_at).select(:id, :created_at, :buy_price, :sell_price).last(100).collect{|bitcoin| [bitcoin.created_at.strftime('%l:%M %p'), bitcoin.buy_price / 100, bitcoin.sell_price / 100]}
+		@bitcoins.insert(0, ['Time', 'Buy amount', 'Sell amount'])
 	end
 
 end
--- app/models/bitcoin.rb
+++ app/models/bitcoin.rb
@@ -1,5 +1,4 @@
 class Bitcoin < ActiveRecord::Base
-
 	def self.fetch_prices		
 		coinbase = Coinbase::Client.new(nil)
 		Bitcoin.create buy_price: coinbase.buy_price.cents, sell_price: coinbase.sell_price.cents
