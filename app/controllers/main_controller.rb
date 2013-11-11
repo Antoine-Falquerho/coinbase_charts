ca1d0200f5432dfffe2965eb63093b0f29197374
Antoine Falquerho <antoine.falquerho@gmail.com>
2013-11-10 22:09:43 -0800

    add best profit of the last 24h and show last 24h records only

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,7 @@
 class MainController < ApplicationController
 
 	def index
-		@bitcoins = Bitcoin.all.order(:created_at).select(:id, :created_at, :buy_price, :sell_price).limit(100)
+		@bitcoins = Bitcoin.all.order(:created_at).select(:id, :created_at, :buy_price, :sell_price).where('created_at >= ?', 24.hours.ago)
 		@to_buy = @bitcoins.order(:buy_price).first
 		@to_sell = @bitcoins.order(:sell_price).last
 		@chart = @bitcoins.collect{|bitcoin| [bitcoin.created_at.strftime('%l:%M %p'), bitcoin.buy_price.to_f / 100, bitcoin.sell_price.to_f / 100]}.insert(0, ['Time', 'Buy amount', 'Sell amount'])		
