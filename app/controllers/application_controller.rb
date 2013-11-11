1565503442ae56b0a1f8252aa71faa83fa400cbd
Antoine Falquerho <antoine.falquerho@gmail.com>
2013-11-10 18:07:47 -0800

    add bootstrap, table with price movement

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :exception  
+  helper_method :current_price
+
+  private  
+
+  def current_price
+  	@current_price = Bitcoin.order(:created_at).select(:created_at, :buy_price, :sell_price).last
+  end
 end
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,8 +1,8 @@
 class MainController < ApplicationController
 
 	def index
-		@bitcoins = Bitcoin.all.order(:created_at).select(:id, :created_at, :buy_price, :sell_price).last(100).collect{|bitcoin| [bitcoin.created_at.strftime('%l:%M %p'), bitcoin.buy_price / 100, bitcoin.sell_price / 100]}
-		@bitcoins.insert(0, ['Time', 'Buy amount', 'Sell amount'])
+		@bitcoins = Bitcoin.all.order(:created_at).select(:id, :created_at, :buy_price, :sell_price).last(100)
+		@chart = @bitcoins.collect{|bitcoin| [bitcoin.created_at.strftime('%l:%M %p'), bitcoin.buy_price.to_f / 100, bitcoin.sell_price.to_f / 100]}.insert(0, ['Time', 'Buy amount', 'Sell amount'])		
 	end
 
 end
